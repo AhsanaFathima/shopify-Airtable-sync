2713f280d0123c27d3529cb468d279890c823379
AhsanaFathima <ahsanafathimae444@gmail.com>
2026-01-03 19:37:34 +0530

    update indentation error

--- webhook_app.py
+++ webhook_app.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 print("🚀 Flask app starting...", flush=True)
 
 # ---------- ENV ----------
-# ---------- ENV ----------
 SHOP = os.getenv("SHOPIFY_SHOP")
 TOKEN = os.getenv("SHOPIFY_API_TOKEN")
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
@@ -15,8 +14,10 @@ API_VERSION = os.getenv("SHOPIFY_API_VERSION", "2024-07")
 
 if not SHOP:
     print("⚠️ SHOPIFY_SHOP not set", flush=True)
+
 if not TOKEN:
     print("⚠️ SHOPIFY_API_TOKEN not set", flush=True)
+
 if not WEBHOOK_SECRET:
     #print("⚠️ WEBHOOK_SECRET not set", flush=True)
 
